c401fbfd698832040ffec7b4853500d8e79eb51f
Clemens Schulz <clemens@wetfish.de>
2019-07-04 00:18:07 +0200

    Added JSON matcher

--- Hippolyte/Matcher.swift
+++ Hippolyte/Matcher.swift
@@ -112,3 +112,38 @@ public class DataMatcher: Matcher {
   }
 
 }
+
+public class JsonMatcher<T: Decodable & Hashable>: Matcher {
+
+  let decoder: JSONDecoder
+  let object: T
+
+  public init(object: T) {
+    self.decoder = JSONDecoder()
+    self.object = object
+  }
+
+  public override func matches(data: Data?) -> Bool {
+    guard let data = data else {
+      return false
+    }
+
+    guard let decodedObject = try? self.decoder.decode(T.self, from: data) else {
+      return false
+    }
+
+    return self.object == decodedObject
+  }
+
+  public override func hash(into hasher: inout Hasher) {
+    hasher.combine(self.object)
+  }
+
+  override func isEqual(to other: Matcher) -> Bool {
+    if let theOther = other as? JsonMatcher<T> {
+      return theOther.object == self.object
+    }
+    return false
+  }
+
+}
