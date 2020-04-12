97201578b84be86d32ed94c5ce516b8546b86c27
Nikita Gorelikov <nikita.gorelikov@easybrain.com>
2020-04-12 11:49:54 +0300

    one more open fix

--- Hippolyte/Matchers/Matcher.swift
+++ Hippolyte/Matchers/Matcher.swift
@@ -6,19 +6,19 @@ import Foundation
 
 open class Matcher: Hashable {
 
-  public func matches(string: String?) -> Bool {
+  open func matches(string: String?) -> Bool {
     false
   }
 
-  public func matches(data: Data?) -> Bool {
+  open func matches(data: Data?) -> Bool {
     false
   }
 
-  public func isEqual(to other: Matcher) -> Bool {
+  open func isEqual(to other: Matcher) -> Bool {
     false
   }
 
-  public func hash(into hasher: inout Hasher) {
+  open func hash(into hasher: inout Hasher) {
   }
 
   public static func ==(lhs: Matcher, rhs: Matcher) -> Bool {
